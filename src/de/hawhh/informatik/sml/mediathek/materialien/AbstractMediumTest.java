62356bbcec636ac407622bca6ee0d0242b705587
acd058 <acd058@HODOR.INFORMATIK>
2017-06-01 08:14:42 +0200

    paket namen geändert

---
+++ src/de/hawhh/informatik/sml/mediathek/materialien/AbstractMediumTest.java
@@ -0,0 +1,39 @@
+package de.hawhh.informatik.sml.mediathek.materialien;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public abstract class AbstractMediumTest
+{
+
+    protected static final String KOMMENTAR = "Kommentar";
+    protected static final String TITEL = "Titel";
+    private Medium _medium1;
+    private Medium _medium2;
+    
+    protected AbstractMediumTest()
+    {
+    	_medium1 = getMedium();
+    	_medium2 = getMedium();
+    }
+    
+    protected abstract Medium getMedium();
+    
+    @Test
+    public abstract void testBerechneMietgebuehr();
+    
+    @Test
+    public void testGetFormatiertenString()
+    {
+        Medium medium = getMedium();
+        assertNotNull(medium.getFormatiertenString());
+    }
+    
+    public void testEquals()
+    {
+        assertFalse("Mehrere Exemplare des gleichen Mediums sind ungleich",
+                _medium1.equals(_medium2));
+        assertTrue("Dasselbe Exemplare des gleichen Mediums ist gleich",
+                _medium1.equals(_medium1));
+    }
+}
---
+++ src/de/hawhh/informatik/sml/mediathek/service/ServiceObserver.java
@@ -0,0 +1,20 @@
+package de.hawhh.informatik.sml.mediathek.service;
+/**
+ * Interface für Beobachter, die sich für Änderungen eines ObservableService
+ * interessieren.
+ * 
+ * @author SE2-Team, PM2-Team
+ * @version SoSe 2017
+ */
+public interface ServiceObserver
+{
+
+    /**
+     * Diese Operation wird aufgerufen, sobald sich an an dem beobachteten
+     * Service etwas relevantes geändert hat.
+     * 
+     * Implementierende Klassen müssen in dieser Operation auf die Aenderung
+     * reagieren.
+     */
+    void reagiereAufAenderung();
+}
